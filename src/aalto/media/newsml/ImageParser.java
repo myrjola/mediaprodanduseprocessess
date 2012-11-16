b57d9cce6a3d29f75e992842a5022d53667362c9
myrjola <martin.yrjola@relex.fi>
2012-11-16 13:31:59 +0200

    Implement retrieving of guid.

--- src/aalto/media/newsml/ImageParser.java
+++ src/aalto/media/newsml/ImageParser.java
@@ -6,11 +6,16 @@ import java.io.IOException;
 import org.apache.sanselan.ImageReadException;
 import org.apache.sanselan.Sanselan;
 
+import com.adobe.xmp.XMPConst;
+import com.adobe.xmp.XMPException;
+import com.adobe.xmp.XMPMeta;
+import com.adobe.xmp.XMPMetaFactory;
+import com.adobe.xmp.impl.XMPMetaParser;
+
 public class ImageParser {
 
-    public static String readXMP(String filename)
-    {
-        //sanselan to read xmp    
+    public static String readXMP(String filename) {
+        //sanselan to read xmp
         File imagefile = new File(filename);
 
         try {
@@ -25,9 +30,16 @@ public class ImageParser {
 
     }
 
-    public static String getNewsItemGuid(String xmp)
-    {
-        return "";
+    public static String getNewsItemGuid(String xmp) {
+        try {
+            XMPMeta meta = XMPMetaFactory.parseFromString(xmp);
+            return meta.getProperty(XMPConst.NS_DC, "identifier").toString();
+        } catch (XMPException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return "";
+        }
+
 
     }
 
@@ -42,7 +54,7 @@ public class ImageParser {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println(ImageParser.readXMP(args[0]));
+        System.out.println(getNewsItemGuid(ImageParser.readXMP(args[0])));
 
     }
 
