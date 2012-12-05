ca815cc8607a550a33fc1dd4433da66884449c6e
myrjola <martin.yrjola@relex.fi>
2012-12-05 17:37:35 +0200

    Add Assignment 4 project

---
+++ MysteriousOstrich/src/com/example/mysteriousostrich/MysteriousostrichApplication.java
@@ -0,0 +1,15 @@
+package com.example.mysteriousostrich;
+
+import com.vaadin.Application;
+import com.vaadin.ui.*;
+
+public class MysteriousostrichApplication extends Application {
+	@Override
+	public void init() {
+		Window mainWindow = new Window("Mysteriousostrich Application");
+		Label label = new Label("Hello Vaadin user");
+		mainWindow.addComponent(label);
+		setMainWindow(mainWindow);
+	}
+
+}
