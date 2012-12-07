594f302bbbf86235c869bb654462659f624dd770
mrrej89 <mr.rejstrom@gmail.com>
2012-12-07 17:05:42 +0200

    added startpage

---
+++ Newstopia/src/vaadin/ui/Startpage.java
@@ -0,0 +1,38 @@
+package vaadin.ui;
+
+import vaadin.main.window.NewsItemDisplayer;
+
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
+
+public class Startpage extends Panel {
+	
+	    private Label article;
+	    private NewsItemDisplayer app;
+
+	    public Startpage(final NewsItemDisplayer app) {
+	        this.app = app;
+	        //String test = string;
+	        addStyleName("start");
+
+	        setCaption("Welcome");
+	        setSizeFull();
+
+	        /* Use a FormLayout as main layout for this Panel */
+	        VerticalLayout Layout = new VerticalLayout();
+	        setContent(Layout);
+
+	        /* Create UI components */
+	        article = new Label("Start page duh",Label.CONTENT_XHTML);
+
+	        /* Add all the created components to the form */
+	        addComponent(article);
+	        //addComponent(fieldToSearch);
+	        //addComponent(saveSearch);
+	        //addComponent(searchName);
+	        //addComponent(search);
+	    }
+
+	}
+
