bae35201bd7c0d0c81b24b6ac7bbea13a5309931
myrjola <martin.yrjola@relex.fi>
2012-12-07 15:55:31 +0200

    Get PackageItem to tree

--- Newstopia/src/vaadin/ui/NavigationTree.java
+++ Newstopia/src/vaadin/ui/NavigationTree.java
@@ -2,18 +2,23 @@ package vaadin.ui;
 
 import vaadin.main.window.NewsItemDisplayer;
 
+import aalto.media.newsml.PackageGenerator;
+import aalto.media.newsml.PackageItem;
+
 import com.vaadin.event.ItemClickEvent.ItemClickListener;
 import com.vaadin.ui.Tree;
 
 @SuppressWarnings("serial")
 public class NavigationTree extends Tree {
-    public static final Object PACKAGE1 = "Package Item 1";
+	public static PackageGenerator pg;
+	public static Object PACKAGE1;
     public static final Object PACKAGE2 = "Package Item 2";
 
     public NavigationTree(NewsItemDisplayer app) {
+    	pg = new PackageGenerator("/public/pictureitems");
+    	PACKAGE1 = pg.generatePackage();
         addItem(PACKAGE1);
         addItem(PACKAGE2);
-        
 
         /*
          * We want items to be selectable but do not want the user to be able to
